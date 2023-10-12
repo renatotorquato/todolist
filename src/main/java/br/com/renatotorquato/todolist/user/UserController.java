40aef9a3f28a3dad17311d41a7efdf7c7afd0835
renatotorquato <renatotorquato2011@gmail.com>
2023-10-12 20:19:35 -0300

    second commit

--- src/main/java/br/com/renatotorquato/todolist/user/UserController.java
+++ src/main/java/br/com/renatotorquato/todolist/user/UserController.java
@@ -1,5 +1,8 @@
 package br.com.renatotorquato.todolist.user;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    //com o Autowired eu estou pedindo para o Spring gerenciar o ciclo de vida do IUserRepository
+    @Autowired
+    private IUserRepository userRepository;
     // quando coloco @requestBody significa que os dados virão no corpo da requisição.
      @PostMapping("/")
-    public void create(@RequestBody UserModel userModel){
-        System.out.println(userModel.name);
+    public ResponseEntity create(@RequestBody UserModel userModel){
+        var user = this.userRepository.findByUsername(userModel.getUsername());
+        if(user != null){
+            System.out.println("Usuário já existe");
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Usuário já existe");
+        }
+        var userCreated = this.userRepository.save(userModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(userCreated);
     }
 }
--- src/main/java/br/com/renatotorquato/todolist/user/UserModel.java
+++ src/main/java/br/com/renatotorquato/todolist/user/UserModel.java
@@ -1,8 +1,34 @@
 package br.com.renatotorquato.todolist.user;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+//Apenas adicionando @Data o Lombok(biblioteca do java) adiciona automaticamente os GEtters e Setters
+@Data
+@Entity(name = "tb_users")
+
 public class UserModel {
-    //estes são os atributos da classe e quando não definoi o tipo de modificador public não funcionou.
-   public String name;
-   public String username;
-   public String password;
+    // estes são os atributos da classe e quando não definoi o tipo de modificador
+    // public não funcionou.
+    // na segunda aula mudou para private e ensinou como usar o setters e getters
+    // para acessar e alterar estes atributos
+    @Id
+    @GeneratedValue(generator = "UUID")
+    private UUID id;
+
+    @Column(unique = true)
+    private String name;
+    private String username;
+    private String password;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
